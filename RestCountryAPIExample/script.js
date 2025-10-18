460c308ae522dc900fb31f6dcd599963edfc7754
Poojii001 <poojapal5781@gmail.com>
2025-10-18 12:10:40 +0530

    RestCountryAPI

--- RestCountryAPIExample/script.js
+++ RestCountryAPIExample/script.js
@@ -2,68 +2,75 @@ var center = document.querySelector(".center")
 var second = document.querySelector(".second")
 var input = document.querySelector("#name")
 
-function generate(key,value){
+function generate(key, value) {
     let itemsDiv = document.createElement("div")
     itemsDiv.classList.add("items")
 
     let keyDiv = document.createElement("div")
     keyDiv.classList.add("key")
+    keyDiv.innerHTML = key
 
-    if(key==="flags"){
+    let valueDiv = document.createElement("div")
+    valueDiv.classList.add("value")
+
+
+    if (key === "flags") {
         let img = document.createElement("img")
         img.src = value
-        keyDiv.appendChild(img)
+        valueDiv.appendChild(img)
     }
-    else if(key==="maps"){
+    else if (key === "maps") {
         let a = document.createElement("a")
         a.href = value
         a.innerHTML = "Click here to open google map"
         a_target = "_blank"
-        keyDiv.appendChild(a)
+        valueDiv.appendChild(a)
     }
     else
-    keyDiv.innerHTML = key
-
-    let valueDiv = document.createElement("div")
-    valueDiv.classList.add("key")
-    valueDiv.innerHTML = value
+        valueDiv.innerHTML = value
 
     itemsDiv.appendChild(keyDiv)
     itemsDiv.appendChild(valueDiv)
     second.append(itemsDiv)
 }
 
-function getAPIData(){
+function getAPIData() {
     let name = "bharat"
 
-    if(Input.value !== "") {
+    if (input.value !== "") {
         name = input.value
         input.value = ""
     }
 
     let request = new XMLHttpRequest()
-    request.open("get","https://restcountries.com/v3.1/name/bharat")
+    request.open("get", "https://restcountries.com/v3.1/name/" +name)
     request.send()
 
-    request.addEventListener("load",()=>{
+    request.addEventListener("load", () => {
         let data = JSON.parse(request.responseText)
+
+        center.removeChild(second)
+        second = document.createElement("div")
+        second.classList.add("second")
+        center.appendChild(second)
+
         data.forEach(x => {
-            console.log("name",x.name.official)
-            console.log("capital",x.capital)
-            console.log("flags",x.flags.png)
-            console.log("population",x.population)
-            console.log("area",x.area)
-            console.log("region",x.region)
-            console.log("subregion",x.subregion)
-            console.log("continents",x.continents)
-            console.log("borders",x.borders)
-            console.log("timezone",x.timezone)
-            console.log("landlocked",x.landlocked)
-            console.log("independent",x.independent)
-            console.log("unMember",x.unMember)
-            console.log("maps",x.maps.googleMaps)
-            console.log("langua",x.langua)
-            console.log("currencies",x.currencies)
+            generate("name", x.name.official)
+            generate("capital", x.capital)
+            generate("flags", x.flags.png)
+            generate("population", x.population)
+            generate("area", x.area)
+            generate("region", x.region)
+            generate("subregion", x.subregion)
+            generate("continents", x.continents)
+            generate("borders", x.borders)
+            generate("timezone", x.timezone)
+            generate("landlocked", x.landlocked)
+            generate("independent", x.independent)
+            generate("unMember", x.unMember)
+            generate("maps", x.maps.googleMaps)
+            generate("language", Object.values(x.language))
+            generate("currencies", Object.values(Object.values(x.currencies)[0]))
 
         });
     })
