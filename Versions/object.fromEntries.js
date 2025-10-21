a84900325701c43763e05c91fdbb20bd078115eb
Poojii001 <poojapal5781@gmail.com>
2025-10-21 18:20:38 +0530

    Versions

---
+++ Versions/object.fromEntries.js
@@ -0,0 +1,10 @@
+var data = [
+    ['id' , 1001],
+    ['name' ,'Pooja'],
+    ['dsg','Student'],
+    ['salary',2656363],
+    ['city' , 'Lucknow'],
+    ['state', 'UP']
+]
+console.log(data)
+console.log(Object.fromEntries(data))
